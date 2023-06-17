d332690f167b76d68f36496b78ba96e080c86090
jaime-rey <jaimereycasado@gmail.com>
2023-06-18 00:02:49 +0200

    tuto04

--- 03tut/index.js
+++ 03tut/index.js
@@ -1,2 +1,6 @@
 const {format }= require('date-fns');
-console.log(format(new Date(), 'yyyy-MM-dd HH:mm:ss'));1
+const {v4: uuid} = require('uuid');
+console.log("hello world");
+console.log(format(new Date(), 'yyyy-MM-dd HH:mm:ss'));
+console.log(uuid());
+console.log(uuid());
